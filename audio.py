437b8621ccd6024daf7ea467e4abb44117f42aa0
qwertyadrian <qwertyadrian@yandex.ru>
2018-09-18 21:59:00 +0300

    Первая версия программы.

---
+++ audio.py
@@ -0,0 +1,344 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" 
+  Copyright (C) 2018 Adrian Polyakov
+
+  This file is part of VkMusic Downloader
+  
+  VkMusic Downloader free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see http://www.gnu.org/licenses/
+"""
+import sys
+import os
+import os.path
+import getpass
+import codecs
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QThread, pyqtSignal
+import audio_gui
+from vk_api import VkApi, exceptions
+from vk_api.audio_url_decoder import decode_audio_url
+from bs4 import BeautifulSoup
+from wget import download
+from re import sub
+
+
+class GetAudioListThread(QThread):
+    signal = pyqtSignal('PyQt_PyObject')
+    str_signal = pyqtSignal(str)
+
+    def __init__(self):
+        QThread.__init__(self)
+        self.login = ''
+        self.password = ''
+        self.user_link = ''
+        self.statusInfo = None
+
+    def __del__(self):
+        self.wait()
+
+    def _get_user_audio(self, user_login, user_password, userlink):
+        tracks = []
+        offset = 0
+        session = VkApi(login=user_login, password=user_password, auth_handler=self.auth_handler,
+                        config_filename=cookie)
+        self.statusInfo.setText('Авторизация.')
+        session.auth()
+        api_vk = session.get_api()
+        session.http.cookies.update(dict(remixmdevice='1920/1080/1/!!-!!!!'))
+        user_id = userlink.replace('https://vk.com/', '').replace('https://m.vk.com/', '')
+        me = api_vk.users.get()[0]
+        try:
+            id = api_vk.users.get(user_ids=user_id)[0]
+        except IndexError:
+            id = me
+        url = 'https://m.vk.com/audios{}'.format(id['id'])
+        self.statusInfo.setText('Получение списка аудиозаписей пользователя: {} {}'.format(id['first_name'],
+                                                                                           id['last_name']))
+        while True:
+            response = session.http.get(url, params={'offset': offset}, allow_redirects=False)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            temp = []
+            for audio in soup.find_all('div', {'class': 'audio_item'}):
+                # noinspection PyBroadException
+                try:
+                    artist = audio.select_one('.ai_artist').text
+                    title = audio.select_one('.ai_title').text
+                    duration = int(audio.select_one('.ai_dur')['data-dur'])
+                    link = audio.select_one('.ai_body').input['value']
+                except Exception:
+                    continue
+                if 'audio_api_unavailable' in link:
+                    link = decode_audio_url(link, me['id'])
+                temp.append({'artist': artist, 'title': title, 'duration': duration, 'link': link})
+            tracks += temp[:-6]
+            if int(soup.find_all('div', {'class': 'audioPage__count'})[0].string.split()[0]) <= offset:
+                break
+            offset += 50
+        return tracks, soup.title.string
+    
+    def auth_handler(self):
+        """
+        При двухфакторной аутентификации вызывается эта функция.
+        :return: key, remember_device
+        """
+        self.str_signal.emit('Введите код авторизации:')
+        while not window.key:
+            pass
+        return window.key, True
+
+    def run(self):
+        try:
+            result = self._get_user_audio(self.login, self.password, self.user_link)
+            self.signal.emit(result)
+        except exceptions.BadPassword:
+            self.signal.emit('Неверный логин или пароль.')
+        except exceptions.LoginRequired:
+            self.signal.emit('Требуется логин.')
+        except exceptions.PasswordRequired:
+            self.signal.emit('Требуется пароль.')
+        except IndexError:
+            self.signal.emit('Невозможно получить список аудиозаписей. Проверьте, открыты ли они у пользователя.')
+        except exceptions.ApiError as e:
+            if '113' in str(e):
+                self.signal.emit('Неверная ссылка на профиль пользователя (неверный ID пользователя).')
+            else:
+                self.signal.emit(str(e))
+        except Exception as e:
+            print(type(e))
+            self.signal.emit(str(e))
+
+
+class DownloadAudio(QThread):
+    signal = pyqtSignal('PyQt_PyObject')
+    int_signal = pyqtSignal(int)
+
+    def __init__(self):
+        QThread.__init__(self)
+        self.statusInfo = None
+        self.progressBar = None
+        self.tracks = None
+        self.directory = None
+
+    def __del__(self):
+        self.wait()
+
+    def _download_audio(self, track_list, directory):
+        os.chdir(directory)
+        n = 0
+        for track in track_list:
+            name = '%(artist)s - %(title)s.mp3' % track
+            name = sub(r"[/\"?:|<>*]", '', name)
+            if len(name) > 127:
+                name = name[:126]
+            self.statusInfo.setText('Скачивается {}'.format(name))
+            download(track['link'], out=name, bar=None)
+            n += 1
+            self.change_progress(n)
+        os.chdir('../')
+        return 'Скачивание завершено'
+    
+    def run(self):
+        result = self._download_audio(self.tracks, self.directory)
+        self.signal.emit(result)
+
+    def change_progress(self, n):
+        self.int_signal.emit(n)
+    
+
+# noinspection PyArgumentList,PyCallByClass
+class VkAudioApp(QtWidgets.QMainWindow, audio_gui.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        
+        self.btnConfirm.clicked.connect(self.start)
+        self.saveAll.clicked.connect(self.save_all)
+        self.saveWithoutLinks.clicked.connect(self.save_without_links)
+        self.downloadAll.clicked.connect(self.download_all)
+        self.downloadSelected.clicked.connect(self.download_selected)
+        
+        self.get_audio = GetAudioListThread()
+        self.get_audio.signal.connect(self.finished)
+        self.get_audio.str_signal.connect(self.auth_handler)
+        
+        self.download_audio = DownloadAudio()
+        self.download_audio.signal.connect(self.done)
+        self.download_audio.int_signal.connect(self.change_progress)
+        
+        if info:
+            self.login.setText(info[0])
+            self.password.setText(info[1])
+            self.user_link.setText(info[2])
+        
+        self.tracks = None
+        self.string = None
+        self.key = None
+
+    def auth_handler(self, result):
+        self.key = None
+        num, ok = QtWidgets.QInputDialog.getText(self, 'Двухфакторная аутентификация', result)
+        if ok:
+            self.key = num
+
+    def start(self):
+        if self.saveData.isChecked():
+            with open(config, 'wb') as d:
+                data = self.login.text() + '|' + self.password.text() + '|' + self.user_link.text()
+                data_crypted = codecs.encode(bytes(data, 'utf-8'), 'hex')
+                d.write(data_crypted)
+        if self.enableSorting.isChecked():
+            self.trackList.setSortingEnabled(True)
+        else:
+            self.trackList.setSortingEnabled(False)
+        self.get_audio.login = self.login.text()
+        self.get_audio.password = self.password.text()
+        self.get_audio.user_link = self.user_link.text()
+        self.get_audio.statusInfo = self.statusInfo
+        self.btnConfirm.setEnabled(False)
+        self.downloadAll.setEnabled(False)
+        self.saveAll.setEnabled(False)
+        self.saveWithoutLinks.setEnabled(False)
+        self.downloadSelected.setEnabled(False)
+        self.trackList.clear()
+        self.statusInfo.setText('Процесс получение аудиозаписей начался.\n')
+        self.get_audio.start()
+        
+    def finished(self, result):
+        if result and isinstance(result, tuple):
+            self.tracks = result[0]
+            self.string = result[1]
+            self.btnConfirm.setEnabled(True)
+            self.statusInfo.setText('Список аудиозаписей получен.\n{}, {} шт.'.format(self.string, len(self.tracks)))
+            #row = 0
+            self.saveAll.setEnabled(True)
+            self.saveWithoutLinks.setEnabled(True)
+            self.downloadSelected.setEnabled(True)
+            self.downloadAll.setEnabled(True)
+            self.trackList.setEnabled(True)
+            self.btnConfirm.setEnabled(True)
+            for track in self.tracks:
+                self.trackList.addItem('%(artist)s — %(title)s' % track)
+                #self.trackList.setRowCount(len(self.tracks))
+                #self.trackList.setItem(row, 0, QtWidgets.QTableWidgetItem(track['artist']))
+                #self.trackList.setItem(row, 1, QtWidgets.QTableWidgetItem(track['title']))
+                #row += 1
+            #self.trackList.resizeColumnsToContents()
+        elif isinstance(result, str):
+            self.btnConfirm.setEnabled(True)
+            self.statusInfo.setText('\nОшибка: ' + result)
+    
+    def save_all(self):
+        directory = QtWidgets.QFileDialog.getSaveFileName(self, 'Сохранить как', filter='Text files (*.txt)')[0]
+        if not directory.endswith('.txt'):
+            directory += '.txt'
+        if directory and self.tracks and self.string:
+            with open(directory, 'w', encoding='utf-8') as d:
+                print('{}, {} шт.\n'.format(self.string, len(self.tracks)), file=d)
+                for track in self.tracks:
+                    print('%(artist)s - %(title)s: %(link)s\n' % track, file=d)
+            self.statusInfo.setText('Список аудиозаписей сохранен в файл {}'.format(directory))
+    
+    def save_without_links(self):
+        directory = QtWidgets.QFileDialog.getSaveFileName(self, 'Сохранить как', filter='Text files (*.txt)')[0]
+        if not directory.endswith('.txt'):
+            directory += '.txt'
+        if directory and self.tracks and self.string:
+            with open(directory, 'w', encoding='utf-8') as d:
+                print('{}, {} шт.\n'.format(self.string, len(self.tracks)), file=d)
+                for track in self.tracks:
+                    print('%(artist)s - %(title)s' % track, file=d)
+            self.statusInfo.setText('Список аудиозаписей (без ссылок на скачивание) сохранен в файл {}'
+                                    .format(directory))
+
+    def download_all(self):
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+        if directory:
+            self.download_audio.statusInfo = self.statusInfo
+            self.download_audio.tracks = self.tracks
+            self.download_audio.directory = directory
+            self.statusInfo.setText('Процесс скачивания аудиозаписей начался.')
+            self.progress_label.setEnabled(True)
+            self.progressBar.setEnabled(True)
+            self.progressBar.setMaximum(len(self.tracks))
+            self.downloadAll.setEnabled(False)
+            self.saveAll.setEnabled(False)
+            self.saveWithoutLinks.setEnabled(False)
+            self.downloadSelected.setEnabled(False)
+            self.btnConfirm.setEnabled(False)
+            self.download_audio.start()
+
+    def download_selected(self):
+        selected = self.trackList.selectedItems()
+        selected_tracks = []
+        for element in selected:
+            for track in self.tracks:
+                if element.text() in '%(artist)s — %(title)s' % track:
+                    selected_tracks.append(track)
+                    break
+        if selected_tracks:
+            directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+        if directory:
+            self.download_audio.statusInfo = self.statusInfo
+            self.download_audio.tracks = selected_tracks
+            self.download_audio.directory = directory
+            self.statusInfo.setText('Процесс скачивания аудиозаписей начался.')
+            self.progress_label.setEnabled(True)
+            self.progressBar.setEnabled(True)
+            self.progressBar.setMaximum(len(selected_tracks))
+            self.downloadAll.setEnabled(False)
+            self.saveAll.setEnabled(False)
+            self.saveWithoutLinks.setEnabled(False)
+            self.downloadSelected.setEnabled(False)
+            self.btnConfirm.setEnabled(False)
+            self.download_audio.start()
+            
+    
+    def done(self, result):
+        self.downloadAll.setEnabled(True)
+        self.saveAll.setEnabled(True)
+        self.saveWithoutLinks.setEnabled(True)
+        self.downloadSelected.setEnabled(True)
+        self.btnConfirm.setEnabled(True)
+        self.statusInfo.setText(result)
+
+    def change_progress(self, result):
+        self.progressBar.setValue(result)
+        
+
+def ui():
+    global window
+    app = QtWidgets.QApplication(sys.argv)
+    window = VkAudioApp()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    info = None
+    user = ''
+    if os.name == 'posix':
+        user = ''
+    elif os.name == 'nt':
+        user = getpass.getuser()
+    home = os.path.join(os.path.expanduser('~' + user), '.vk_downloader')
+    config = os.path.join(home, '.config.ini')
+    cookie = os.path.join(home, 'vk_cookies.json')
+    if os.path.exists(home):
+        if os.path.exists(config):
+            with open(config, 'rb') as f:
+                info_crypted = f.read()
+                info = codecs.decode(info_crypted, 'hex').decode().split('|')
+    else:
+        os.mkdir(home)
+    ui()
---
+++ audio_gui.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+"""  
+  Copyright (C) 2018 Adrian Polyakov
+  
+  This file is part of VkMusic Downloader
+
+  VkMusic Downloader is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see http://www.gnu.org/licenses/
+"""
+# Form implementation generated from reading ui file 'audio.ui'
+#
+# Created by: PyQt5 UI code generator 5.7
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import os.path
+import sys
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setEnabled(True)
+        MainWindow.resize(780, 480)
+        MainWindow.setMinimumSize(QtCore.QSize(780, 480))
+        MainWindow.setMaximumSize(QtCore.QSize(780, 480))
+        icon = QtGui.QIcon()
+        logo = resource_path("logo.png")
+        icon.addPixmap(QtGui.QPixmap(logo), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.login = QtWidgets.QLineEdit(self.centralwidget)
+        self.login.setGeometry(QtCore.QRect(340, 10, 181, 31))
+        self.login.setObjectName("login")
+        self.login_text = QtWidgets.QLabel(self.centralwidget)
+        self.login_text.setGeometry(QtCore.QRect(10, 10, 331, 31))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.login_text.setFont(font)
+        self.login_text.setObjectName("login_text")
+        self.password_text = QtWidgets.QLabel(self.centralwidget)
+        self.password_text.setGeometry(QtCore.QRect(270, 50, 61, 31))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.password_text.setFont(font)
+        self.password_text.setObjectName("password_text")
+        self.password = QtWidgets.QLineEdit(self.centralwidget)
+        self.password.setGeometry(QtCore.QRect(340, 50, 181, 31))
+        self.password.setInputMethodHints(QtCore.Qt.ImhHiddenText|QtCore.Qt.ImhNoAutoUppercase|QtCore.Qt.ImhNoPredictiveText|QtCore.Qt.ImhSensitiveData)
+        self.password.setInputMask("")
+        self.password.setText("")
+        self.password.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.password.setDragEnabled(False)
+        self.password.setObjectName("password")
+        self.link_text = QtWidgets.QLabel(self.centralwidget)
+        self.link_text.setGeometry(QtCore.QRect(60, 90, 281, 31))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.link_text.setFont(font)
+        self.link_text.setObjectName("link_text")
+        self.user_link = QtWidgets.QLineEdit(self.centralwidget)
+        self.user_link.setGeometry(QtCore.QRect(340, 90, 181, 31))
+        self.user_link.setObjectName("user_link")
+        self.btnConfirm = QtWidgets.QPushButton(self.centralwidget)
+        self.btnConfirm.setGeometry(QtCore.QRect(590, 440, 181, 29))
+        self.btnConfirm.setAutoDefault(False)
+        self.btnConfirm.setDefault(False)
+        self.btnConfirm.setObjectName("btnConfirm")
+        self.status_label = QtWidgets.QLabel(self.centralwidget)
+        self.status_label.setGeometry(QtCore.QRect(10, 120, 68, 61))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.status_label.setFont(font)
+        self.status_label.setObjectName("status_label")
+        self.saveData = QtWidgets.QCheckBox(self.centralwidget)
+        self.saveData.setEnabled(True)
+        self.saveData.setGeometry(QtCore.QRect(530, 90, 251, 27))
+        self.saveData.setObjectName("saveData")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(10, 410, 331, 21))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.saveAll = QtWidgets.QPushButton(self.centralwidget)
+        self.saveAll.setEnabled(False)
+        self.saveAll.setGeometry(QtCore.QRect(10, 440, 91, 29))
+        self.saveAll.setObjectName("saveAll")
+        self.saveWithoutLinks = QtWidgets.QPushButton(self.centralwidget)
+        self.saveWithoutLinks.setEnabled(False)
+        self.saveWithoutLinks.setGeometry(QtCore.QRect(110, 440, 171, 29))
+        self.saveWithoutLinks.setObjectName("saveWithoutLinks")
+        self.downloadAll = QtWidgets.QPushButton(self.centralwidget)
+        self.downloadAll.setEnabled(False)
+        self.downloadAll.setGeometry(QtCore.QRect(450, 440, 91, 29))
+        self.downloadAll.setObjectName("downloadAll")
+        self.statusInfo = QtWidgets.QLabel(self.centralwidget)
+        self.statusInfo.setGeometry(QtCore.QRect(70, 130, 701, 41))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.statusInfo.setFont(font)
+        self.statusInfo.setWordWrap(True)
+        self.statusInfo.setObjectName("statusInfo")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setEnabled(True)
+        self.label_4.setGeometry(QtCore.QRect(10, 180, 161, 16))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.downloadSelected = QtWidgets.QPushButton(self.centralwidget)
+        self.downloadSelected.setEnabled(False)
+        self.downloadSelected.setGeometry(QtCore.QRect(290, 440, 151, 29))
+        self.downloadSelected.setObjectName("downloadSelected")
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setEnabled(False)
+        self.progressBar.setGeometry(QtCore.QRect(610, 172, 161, 21))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.progressBar.setFont(font)
+        self.progressBar.setMinimum(0)
+        self.progressBar.setMaximum(1)
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setAlignment(QtCore.Qt.AlignCenter)
+        self.progressBar.setTextVisible(True)
+        self.progressBar.setInvertedAppearance(False)
+        self.progressBar.setObjectName("progressBar")
+        self.progress_label = QtWidgets.QLabel(self.centralwidget)
+        self.progress_label.setEnabled(False)
+        self.progress_label.setGeometry(QtCore.QRect(470, 170, 141, 20))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.progress_label.setFont(font)
+        self.progress_label.setObjectName("progress_label")
+        self.trackList = QtWidgets.QListWidget(self.centralwidget)
+        self.trackList.setEnabled(False)
+        self.trackList.setGeometry(QtCore.QRect(10, 200, 761, 211))
+        self.trackList.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.trackList.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
+        self.trackList.setObjectName("trackList")
+        self.enableSorting = QtWidgets.QCheckBox(self.centralwidget)
+        self.enableSorting.setGeometry(QtCore.QRect(530, 46, 171, 41))
+        self.enableSorting.setStatusTip("")
+        self.enableSorting.setTristate(False)
+        self.enableSorting.setObjectName("enableSorting")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "VKMusic Downloader"))
+        self.login_text.setText(_translate("MainWindow", "Номер телефона или электронная почта:"))
+        self.password_text.setText(_translate("MainWindow", "Пароль:"))
+        self.link_text.setText(_translate("MainWindow", "Ссылка на профиль пользователя:"))
+        self.btnConfirm.setToolTip(_translate("MainWindow", "Получить список аудиозаписей пользователя"))
+        self.btnConfirm.setText(_translate("MainWindow", "Получить аудиозаписи"))
+        self.status_label.setText(_translate("MainWindow", "Статус:"))
+        self.saveData.setToolTip(_translate("MainWindow", "Сохранить введенные данные?"))
+        self.saveData.setText(_translate("MainWindow", "Запомнить"))
+        self.label_2.setText(_translate("MainWindow", "Что сделать с полученным списком аудиозаписей:"))
+        self.saveAll.setToolTip(_translate("MainWindow", "Сохранить список аудиозаписей в файл со ссылками для их скачивания"))
+        self.saveAll.setText(_translate("MainWindow", "Сохранить"))
+        self.saveWithoutLinks.setToolTip(_translate("MainWindow", "Сохранить список аудиозаписей в файл без ссылок для их скачивания"))
+        self.saveWithoutLinks.setText(_translate("MainWindow", "Сохранить без ссылок"))
+        self.downloadAll.setToolTip(_translate("MainWindow", "Скачать все аудиозаписи из списка выше"))
+        self.downloadAll.setText(_translate("MainWindow", "Скачать всё"))
+        self.statusInfo.setText(_translate("MainWindow", "Готов к работе."))
+        self.label_4.setText(_translate("MainWindow", "Список аудиозаписей:"))
+        self.downloadSelected.setToolTip(_translate("MainWindow", "Скачать выбранные ауиозаписи из списка выше"))
+        self.downloadSelected.setText(_translate("MainWindow", "Скачать выбранные"))
+        self.progressBar.setFormat(_translate("MainWindow", "Скачано %v из %m"))
+        self.progress_label.setText(_translate("MainWindow", "Прогресс скачивания:"))
+        self.enableSorting.setToolTip(_translate("MainWindow", "Сортировать спсиок в алфавитном порядке?"))
+        self.enableSorting.setText(_translate("MainWindow", "Сортировать список"))
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath('.')
+    return os.path.join(base_path, relative_path)
+    
